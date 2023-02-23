f258f2a473b3eea65aef6067c8fe2eadf168171e
Shayidina Daffa Dasvilla <116255530+Shayidinadapa@users.noreply.github.com>
2023-02-23 13:01:26 +0700

    Add files via upload

---
+++ tugas1a.php
@@ -0,0 +1,19 @@
+<?php
+$Topi = "Topi";
+$muter = "Bundar";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas1</title>
+</head>
+<body>
+    <h2>    
+    <?php echo "\"<strong><em>$Topi saya $muter, $muter $Topi saya.<em></strong>\""; ?>
+    </h2>
+</body>
+</html>
---
+++ tugas1c.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table{
+            background-color:blue;
+        }
+    </style>
+</head>
+<body>
+    <table border="1" cellpading="20" cellspacing="0">
+        <tr>
+            <td>1</td>
+        </tr>
+    </table>
+    <table border="1" cellpading="20" cellspacing="0">
+        <tr>
+            <td>2</td>
+            <td>2</td>
+        </tr> 
+    </table>
+    <table border="1" cellpading="20" cellspacing="0">
+        <tr>
+            <td>3</td>
+            <td>3</td>
+            <td>3</td>
+        </tr>
+    </table>
+</body>
+</html>
